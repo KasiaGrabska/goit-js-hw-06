24fe07c7b956acecb6eb0ae86a5fb84bb1969e51
KasiaGrabska <143888097+KasiaGrabska@users.noreply.github.com>
2024-01-20 13:28:21 +0100

    Part of task four

---
+++ js/task-04.js
@@ -0,0 +1,14 @@
+// początkowa wartość zmiennej
+let counterValue = 0;
+
+// zmienna za pomocą której dostajemy się do span
+const valueSelector = document.querySelector("value");
+
+// zmienna za pomoca której dostajemy się do przycisku
+const incrementButtonAction = document.querySelector(
+  '[data-action="increment]'
+);
+
+const decrementButtonAction = document.querySelector(
+  '[data-action="decrement"]'
+);
