cf262e9b4da6f9eeceb0cec53d0a4b7c2ec4c98a
KasiaGrabska <143888097+KasiaGrabska@users.noreply.github.com>
2024-01-20 11:15:55 +0100

    Second task is ready

--- js/task-01.js
+++ js/task-01.js
@@ -3,9 +3,12 @@ const numberOfCategories = document.querySelectorAll("#categories .item");
 
 console.log(`Number of categories: ${numberOfCategories.length}`);
 
+//iteracja po tablicy element po elemencie
+//wywołanie callback(category) dla kaźdego elementu tablicy
+//nic nie zwraca
 numberOfCategories.forEach((category) => {
   const categoryName = category.querySelector("h2").textContent;
-  const elementsCount = category.querySelectorAll("ul li").length;
+  const categoryElements = category.querySelector("ul li").length;
   console.log(`\nCategory: ${categoryName}`);
-  console.log(`Elements: ${elementsCount}`);
+  console.log(`Elements: ${categoryElements}`);
 });
--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ulIng = document.querySelector("#ingredients");
+
+const liArray = ingredients.map((ingredient) => {
+  const li = document.createElement("li");
+  li.textContent = ingredient;
+  li.classList.add("item");
+  return li;
+});
+ulIng.append(...liArray);
