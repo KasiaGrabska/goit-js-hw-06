a015a83cfc65926bd4cd552dcab1c83069ef9273
KasiaGrabska <143888097+KasiaGrabska@users.noreply.github.com>
2024-01-21 13:53:44 +0100

    Changes in task five

--- js/task-05.js
+++ js/task-05.js
@@ -2,11 +2,13 @@ const nameInput = document.querySelector("#name-input");
 const nameOutput = document.querySelector("#name-output");
 
 //metoda dodawania do zdarzenia input
-
+//() oznacza callback, który jest otrzymywany w locie, czyli wpsiujemy imię i ono się automatycznie wyswietla
 nameInput.addEventListener("input", () => {
-  if (nameInput.value.trim() === "") {
-    emptyOutpu.textContent = "Anonymous";
+  //uywamy value aby tekst wprowadzony przez uytkownika został wprowadzony i wyświetlona, nameInput.value to aktualna wartość wpisana do tego pola, jeśli string jest pusty wyświetli się informacja Anonymous
+  if (nameInput.value === "") {
+    nameOutput.textContent = "Anonymous";
+    //w przeciwnym wypadku zostanie wprowadzona wartość (value) wprowadzanej treści
   } else {
-    nameOutpu.text.Content = nameInput;
+    nameOutput.textContent = nameInput.value;
   }
 });
