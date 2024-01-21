37973fbd7325492dfd99c5468bc4f18959645f50
KasiaGrabska <143888097+KasiaGrabska@users.noreply.github.com>
2024-01-21 13:09:55 +0100

    Task Four is ready

--- js/task-04.js
+++ js/task-04.js
@@ -1,14 +1,29 @@
 // początkowa wartość zmiennej
 let counterValue = 0;
 
-// zmienna za pomocą której dostajemy się do span
-const valueSelector = document.querySelector("value");
-
-// zmienna za pomoca której dostajemy się do przycisku
-const incrementButtonAction = document.querySelector(
-  '[data-action="increment]'
-);
-
+//decrement = ubytek
 const decrementButtonAction = document.querySelector(
   '[data-action="decrement"]'
 );
+
+// zmienna za pomoca której dostajemy się do właściwości i wartości atrybutu button poprzez uzycie nawiasów []
+//increment = przyrost
+const incrementButtonAction = document.querySelector(
+  '[data-action="increment"]'
+);
+
+// zmienna za pomocą której dostajemy się do span
+const valueSelector = document.querySelector("#value");
+
+//zmiejszanie wartości licznika
+decrementButtonAction.addEventListener("click", () => {
+  counterValue -= 1;
+  //za pomocą textContent zminiamy tekst atrybutu span  z 0 w zaleności od tego czy naciskamy na przycisk aby dodać albo usunąć wartość
+  valueSelector.textContent = counterValue;
+});
+
+//zwiększanie wartości licznika
+incrementButtonAction.addEventListener("click", () => {
+  counterValue += 1;
+  valueSelector.textContent = counterValue;
+});
