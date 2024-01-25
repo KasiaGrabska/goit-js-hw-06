d3e8227c772d9beeb442efe47459388238b82f96
KasiaGrabska <143888097+KasiaGrabska@users.noreply.github.com>
2024-01-25 18:57:27 +0100

    Task eight is ready

--- js/task-08.js
+++ js/task-08.js
@@ -2,28 +2,26 @@
 const logForm = document.querySelector(".login-form");
 
 //tworzymy zdarzenie dla submit i callbackiem w postaci funkcji handleSubmit
-logForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-    //event to obiekt a target to cel, w taki sposób doatniemy się do formularza form czyli nasz const logForm
-    const form = event.target;
-    //form to formularz (atrybut), elements to elementy, email to wartość dla właściwosci name (name = "email"), value to wartość wprowadzanych danych
-    const login = form.elements.email.value;
-    //form to formularz (atrybut), elements to elementy, email to wartość dla właściwosci name (name = "password"), value to wartość wprowadzanych danych
-    const password = form.elements.password.value
-    //jeśli pole email i password są puste zwraca komunikat
-    if (login === "" || password === "") {
+logForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  //event to obiekt a target to cel, w taki sposób doatniemy się do formularza form czyli nasz const logForm
+  const form = event.target;
+  //form to formularz (atrybut), elements to elementy, email to wartość dla właściwosci name (name = "email"), value to wartość wprowadzanych danych
+  const login = form.elements.email.value;
+  //form to formularz (atrybut), elements to elementy, email to wartość dla właściwosci name (name = "password"), value to wartość wprowadzanych danych
+  const password = form.elements.password.value;
+  //jeśli pole email i password są puste zwraca komunikat
+  if (login === "" || password === "") {
+    alert("All fields should be filled in!");
+    return;
+  }
+  //deklarujemy obiekt, którhy zawiera właściwości Login i Password, aby odwołać się do zmiennych email i password uzywamy słowa kluczowego this
+  const object = {
+    Login: login,
+    Password: password,
+  };
 
-        alert ("All fields should filled in!");
-        return;
-    }
-    //deklarujemy obiekt, którhy zawiera właściwości Login i Password, aby odwołać się do zmiennych email i password uzywamy słowa kluczowego this
-    const object = {
-        Login: `${this.email.value}`;
-        Password: `${this.password.value}`;
-    };
-
-    console.log(object);
-    // przez zastosowane, form.reset wymazujemy wprowadzone treści
-    logForm.reset();
+  console.log(object);
+  // przez zastosowane, form.reset wymazujemy wprowadzone treści
+  logForm.reset();
 });
-
