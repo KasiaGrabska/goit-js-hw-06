74053a07d3ca913c3173231f5a77e202749ac45c
KasiaGrabska <143888097+KasiaGrabska@users.noreply.github.com>
2024-01-27 10:02:43 +0100

    Task seven correct

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,8 @@
 const fontSize = document.querySelector("#font-size-control");
 const spanText = document.querySelector("#text");
 
+spanText.style.fontSize = fontSize.value + "px";
+
 //wprowadzamy metodę, która ma wywołać zdarzenie i zwrócić callback w postaci zmiany stylu inline na właściwość font-size
 fontSize.addEventListener("input", () => {
   // zmienna której przypisana jest wartosc wielkości czcionki wraz z dodaniem stringa "px" (bez tego czcionka nie ulegnie zmianie)
