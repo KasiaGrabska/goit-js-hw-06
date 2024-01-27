61080e79d8e90cb528fa44c662aff2532eb25d12
KasiaGrabska <143888097+KasiaGrabska@users.noreply.github.com>
2024-01-27 09:59:32 +0100

    Task five correct

--- js/task-05.js
+++ js/task-05.js
@@ -5,7 +5,7 @@ const nameOutput = document.querySelector("#name-output");
 //() oznacza callback, który jest otrzymywany w locie, czyli wpsiujemy imię i ono się automatycznie wyswietla
 nameInput.addEventListener("input", () => {
   //uywamy value aby tekst wprowadzony przez uytkownika został wprowadzony i wyświetlona, nameInput.value to aktualna wartość wpisana do tego pola, jeśli string jest pusty wyświetli się informacja Anonymous
-  if (nameInput.value === "") {
+  if (nameInput.value.trim() === "") {
     nameOutput.textContent = "Anonymous";
     //w przeciwnym wypadku zostanie wprowadzona wartość (value) wprowadzanej treści
   } else {
